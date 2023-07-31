c482e95ea01b45cce66424a5f85b233230e6b70f
Mr-Foxx <115030615+Mr-Foxx@users.noreply.github.com>
2023-07-31 22:48:25 +0300

    add login

--- helpers/Save.Error.js
+++ helpers/Save.Error.js
@@ -1,6 +1,9 @@
 const saveError = (error, data, next) => {
-  error.status = 400;
-  next();
+  const { code, name } = error;
+  const status = code === 11000 && name === 'MongoServerError' ? 409 : 400;
+  error.status = status;
+
+  next(error);
 };
 
 module.exports = saveError;
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -5,25 +5,72 @@ const authRouter = express.Router();
 const { userSingupSchema } = require('../../schemas/user-schema.js');
 
 const User = require('../../models/user.js');
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+dotenv.config();
+
+// const saveError = require('../../helpers/Save.Error.js');
+
+const { JWT_SECRET } = process.env;
 
 authRouter.post('/register', async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const userData = { name, email, password };
+  const { error } = userSingupSchema.validate(userData);
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  userData.password = hashPassword;
+
+  if (error) {
+    return res.status(400).json(error.message);
+  }
+
   try {
-    const { name, email, password } = req.body;
-    const userData = { name, email, password };
-    const { error } = userSingupSchema.validate(userData);
-
-    if (error) {
-      return res.status(400).json(error.message);
-    }
-
     const newUser = await User.create(userData);
     res.status(201).json({
       name: newUser.name,
       email: newUser.email,
     });
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    if (err.code === 11000 && err.name === 'MongoServerError') {
+      err.status = 409;
+      err.message = 'Email in use';
+    } else {
+      err.status = 500;
+    }
+    return res.status(err.status).json(err.message);
   }
+});
+
+authRouter.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      message: 'Email or password is wrong',
+    });
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    return res.status(401).json({
+      message: 'Email or password is wrong',
+    });
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '23h' });
+
+  res.json({
+    token,
+  });
 });
 
 module.exports = authRouter;
