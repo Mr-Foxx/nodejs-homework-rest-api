9625ea900e7b31ccd93a2beb296f6d28911809d9
Mr-Foxx <115030615+Mr-Foxx@users.noreply.github.com>
2023-08-22 21:57:15 +0300

    done

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// const { sendEmail } = require('./helpers/elasticemail.js');
-// const { sendTestEmail } = require('./helpers/nodemailer.js');
 
 dotenv.config();
 const { DB_HOST, PORT } = process.env;
@@ -10,14 +8,6 @@ const { DB_HOST, PORT } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    // const recipient = 'nibac87354@gienig.com';
-    // const subject = 'Hello from your app';
-    // const content = '<strong>This is a test email.</strong>';
-
-    // sendEmail(recipient, subject, content);
-
-    // sendTestEmail();
-
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}!!!`);
       console.log('Database connection successful!!!');
